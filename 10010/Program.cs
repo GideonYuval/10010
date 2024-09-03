09c5946f55c064f5c1e2b924fcce663f9b98c89c
GideonYuval <72265313+giladyuval@users.noreply.github.com>
2024-09-03 17:37:53 +0300

    Add project files.

---
+++ 10010/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Trainer
+    {
+        private string name;
+        private int wid;
+    }
+    public class Training
+    {
+        private int num;
+    }
+
+    public class SpecialT : Training
+    {
+        private Trainer trainer; //note that num is inherited
+        int time;
+
+        public int GetTime()
+        { return time; }
+    }
+
+    public class Client
+    {
+        private int id;
+        private string name;
+        private Training[] visits;
+
+        public int SumSpecialTime()
+        {
+            int st = 0;
+            for (int i = 0; i < visits.Length; i++)
+            {
+                if (visits[i]!=null && visits[i] is SpecialT)
+                    st += ((SpecialT)visits[i]).GetTime();
+            }
+            return st;
+    }
+
+    public class Management
+    {
+        private Trainer[] staff;
+        private Client[] clients;
+
+        public int AtLeastOneSpecial()
+        {
+                int count = 0;
+                foreach (Client client in clients)
+                    if (client!=null && client.SumSpecialTime() > 0) count++;
+                return count;
+        }
+    }
+}
